8314301199b1ba2a12868ac4f83cd00c741a9d54
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-02-11 17:50:47 +0530

    remove boilerplate code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Card name="dummy name" url="dummy url" caption="this is a caption" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Card from "./components/Card.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Card
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>Name: {{ name }}</div>
+    <div>URL: {{ url }}</div>
+    <div>caption: {{ caption }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    name: String,
+    url: String,
+    caption: String
+  }
+};
+</script>
+
+<style scoped></style>>
