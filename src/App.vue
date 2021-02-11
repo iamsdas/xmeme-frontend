3ae19e546f9fc638ed0f9753095757c790d59aa9
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-02-11 23:39:36 +0530

    use debug server

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <Card name="dummy name" url="dummy url" caption="this is a caption" />
+  <div v-for="(result, index) in results" :key="index">
+    <Card
+      :caption="result.caption"
+      :id="result.id"
+      :name="result.name"
+      :url="result.url"
+    />
   </div>
 </template>
 
@@ -11,6 +16,19 @@ export default {
   name: "App",
   components: {
     Card
+  },
+  data() {
+    return {
+      results: []
+    };
+  },
+  mounted() {
+    fetch("http://54.211.242.231:8081/meme")
+      .then(data => data.json())
+      .then(data => {
+        this.results = data;
+        console.log(data);
+      });
   }
 };
 </script>
